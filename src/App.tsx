fb916f22c173ed89dc2adf370ba618596dde3f2c
Jasmin <j.longa@gmx.ch>
2026-07-29 17:23:14 +0200

    Footer angepasst. Impressum Seite raus. auf App.tsx Impressum raus.

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Hero />
       <Project />
       <Info />
-      <Impressum />
+
       <Footer />
     </>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,16 @@ function footer() {
     <>
       <div className=" bg-slate-800 py-6">
         <h3 className="text-10px text-white text-center">
-          Impressum
-          <p className="text-10px text-white text-center ">@ devjasmin 2026</p>
+          <a
+            href="/Impressum.tsx"
+            className="hover:text-white transition-colors"
+          >
+            Impressum
+          </a>
+
+          <span className="text-10px text-white text-center ">
+            @ devjasmin 2026
+          </span>
         </h3>
       </div>
     </>
